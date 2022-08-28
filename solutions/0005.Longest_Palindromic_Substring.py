a6723d89d1a91043929341b6ea78f69c61d9d3c7
martinhmfong <martinhmfong@gmail.com>
2022-08-28 23:35:20 +0800

    climbing stair question

---
+++ solutions/0005.Longest_Palindromic_Substring.py
@@ -0,0 +1,15 @@
+"""
+https://leetcode.com/problems/longest-palindromic-substring/
+
+Input: s = "babad"
+Output: "bab"
+Explanation: "aba" is also a valid answer.
+
+Input: s = "cbbd"
+Output: "bb"
+"""
+
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        pass
---
+++ solutions/0647.Palindromi_Substrings.py
@@ -0,0 +1,16 @@
+"""
+https://leetcode.com/problems/palindromic-substrings/
+
+Input: s = "abc"
+Output: 3
+Explanation: Three palindromic strings: "a", "b", "c".
+
+Input: s = "aaa"
+Output: 6
+Explanation: Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa".
+"""
+
+
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        pass
